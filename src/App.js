4fd1b9a3abf3f2d73aa3ad0628e8cf1a199b975a
Valeria <valeriashust18@gmail.com>
2018-07-04 17:12:13 +0300

    links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {
     Collapse,
@@ -18,43 +18,56 @@ import {
 } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {} from 'immutable';
-import {AddForm} from './AddForm';
+import {Route, Switch, Link} from 'react-router-dom'
 
 
+const Main = () => (
+    <main>
+        <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/add' component={AddForm}/>
+        </Switch>
+    </main>
+);
 
-class App extends Component {
+class Header extends Component {
     constructor(props) {
         super(props);
-        this.toggle = this.toggle.bind(this);
         this.state = {
             isOpen: false
         };
     }
-    
-    toggle() {
+
+    toggle = () => {
         this.setState({
             isOpen: !this.state.isOpen
         });
-    }
-    
-    addItem() {
-        ReactDOM.render(<AddForm />, document.getElementById('container'));
-    }
-    
+    };
+
     render() {
         return (
-            <div>
+            <header>
                 <Navbar color="inverse" light expand="md">
-                    <NavbarBrand href="/">Your list</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
+                    <NavbarBrand><Link to="/" className='link' activeClassName="active" onlyActiveOnIndex={true}>Your
+                        List</Link></NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}/>
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
                             <NavItem>
-                                <NavLink href="">Add Item</NavLink>
+                                <NavLink><Link to="/add" className='link' activeClassName="active">Add
+                                    Item</Link></NavLink>
                             </NavItem>
                         </Nav>
                     </Collapse>
                 </Navbar>
+            </header>)
+    }
+};
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
                 <Jumbotron>
                     <Container id="container">
                         <Row>
@@ -62,17 +75,16 @@ class App extends Component {
                                 <h1>To do:</h1>
                                 <br/>
                                 <p>
-                                   <InputGroup>
+                                    <InputGroup>
                                         <InputGroupAddon addonType="prepend">
-                                          <InputGroupText>
-                                            <Input addon type="checkbox" aria-label="Checkbox for following text input" />
-                                          </InputGroupText>
+                                            <InputGroupText>
+                                                <Input addon type="checkbox"
+                                                       aria-label="Checkbox for following text input"/>
+                                            </InputGroupText>
                                         </InputGroupAddon>
-                                        <Input value="Wash the dishes" />
-                                      </InputGroup>
+                                       <Input value="Wash the dishes"/>
+                                    </InputGroup>
                                 </p>
-                                <br/>
-                                <Button color="success" onClick={this.addItem} >Add item</Button>{' '}
                             </Col>
                         </Row>
                     </Container>
@@ -82,4 +94,63 @@ class App extends Component {
     }
 }
 
-export default App;
+class AddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        };
+    }
+
+    handleChange = (event) => {
+        this.setState({value: event.target.value});
+    };
+
+    addItemToList = () => {
+        alert('A name was submitted: ' + this.state.value);
+
+    };
+
+    render() {
+        return (
+            <div>
+
+                <Jumbotron>
+                    <Container id="container">
+                        <Row>
+                            <Col>
+                                <p>
+                                    <Form>
+                                        <FormGroup>
+                                            <Label for="exampleText"><h3>Add a new item to your list:</h3></Label>
+                                            <Input type="textArea" name="text" id="exampleText"
+                                                   onChange={this.handleChange} value={this.state.value}/>
+                                        </FormGroup>
+                                    </Form>
+                                </p>
+                                <br/>
+                                <Button color="success" onClick={this.addItemToList}>Add item</Button>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Jumbotron>
+            </div>
+
+        )
+    }
+
+
+}
+
+export default class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Main/>
+            </div>
+        );
+    }
+}
+
