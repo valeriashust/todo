84c3951e0451a859d071560113fc54585ccca720
Valeria <valeriashust18@gmail.com>
2018-07-11 16:29:03 +0300

    fetch

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,21 @@
 "use strict";
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-function fetch() {
+function fetch(url, param) {
+    return new Promise(((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
 
+          xhr.open("GET", url);
+            xhr.onload = () => resolve(xhr.responseText);
+            xhr.onerror = () => reject(xhr.statusText);
+            xhr.send();
+
+
+    }));
 }
+
+fetch('./package.json').then(function (value) {
+    console.log(value)
+}, function (reason) {
+    console.log(reason)
+});
