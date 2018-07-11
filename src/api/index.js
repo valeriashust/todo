e64c0cc67a274d248a23986127d039f2f3d1945a
Valeria <valeriashust18@gmail.com>
2018-07-11 16:43:11 +0300

    fetch

--- src/api/index.js
+++ src/api/index.js
@@ -5,16 +5,28 @@ function fetch(url, param) {
     return new Promise(((resolve, reject) => {
         const xhr = new XMLHttpRequest();
 
-          xhr.open("GET", url);
+        if (param) {
+            xhr.open("POST", url);
+            xhr.onload = () => resolve('success');
+            xhr.onerror = () => reject(xhr.statusText);
+            xhr.send(param);
+        }
+        else {
+            xhr.open("GET", url);
             xhr.onload = () => resolve(xhr.responseText);
             xhr.onerror = () => reject(xhr.statusText);
             xhr.send();
-
-
+        }
     }));
 }
 
-fetch('./package.json').then(function (value) {
+fetch('http://www.nbrb.by/API/ExRates/Currencies', JSON.stringify({text: 'wash the dishes'})).then(function (value) {
+    console.log(value)
+}, function (reason) {
+    console.log(reason)
+});
+
+fetch('http://www.nbrb.by/API/ExRates/Currencies').then(function (value) {
     console.log(value)
 }, function (reason) {
     console.log(reason)
