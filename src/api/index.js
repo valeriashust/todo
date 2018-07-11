d9a97ad7948f8a214b0f9ea47f3876c80eeb058a
Valeria <valeriashust18@gmail.com>
2018-07-11 17:31:38 +0300

    fetch

--- src/api/index.js
+++ src/api/index.js
@@ -1,32 +1,36 @@
 "use strict";
-let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-function fetch(url, param) {
+const responses = require("./mock-responses.json");
+
+
+exports.fetch = (url, param) => {
     return new Promise(((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
+        setTimeout(() => {
+            switch (url) {
+                case '/todos': {
+                    resolve(responses.todos)
+                }
+                case '/todos/active': {
+                    resolve(responses.todos.active)
+                }
+                case '/todos/complete': {
+                    resolve(responses.todos.complete)
+                }
+            }
+        }, 5000);
 
-        if (param) {
-            xhr.open("POST", url);
-            xhr.onload = () => resolve('success');
-            xhr.onerror = () => reject(xhr.statusText);
-            xhr.send(param);
-        }
-        else {
-            xhr.open("GET", url);
-            xhr.onload = () => resolve(xhr.responseText);
-            xhr.onerror = () => reject(xhr.statusText);
-            xhr.send();
-        }
+
     }));
 }
 
-fetch('http://www.nbrb.by/API/ExRates/Currencies', JSON.stringify({text: 'wash the dishes'})).then(function (value) {
+
+fetch('/todos').then(function (value) {
     console.log(value)
 }, function (reason) {
     console.log(reason)
 });
 
-fetch('http://www.nbrb.by/API/ExRates/Currencies').then(function (value) {
+fetch('/todos/complete').then(function (value) {
     console.log(value)
 }, function (reason) {
     console.log(reason)
